8158d0d8ab0e845b6a45fc6ee60bb19cbbae6f6e
joyce gong <joyce_gong@brown.edu>
2025-05-03 14:56:05 -0400

    push ehr

--- src/app.py
+++ src/app.py
@@ -12,6 +12,13 @@ FHIR_SERVER_BASE_URL=os.getenv("FHIR_SERVER_BASE_URL")
 username = os.getenv("FHIR_USERNAME")
 password = os.getenv("FHIR_PASSWORD")
 
+fracture_risks = {
+    'no_ad_memantine': 22.87,
+    'ad_only': 25.065,
+    'ad_memantine': 31.53
+}
+
+
 def request_patient(patient_id, credentials):
 
     req = requests.get(FHIR_SERVER_BASE_URL + "/Patient/" + str(patient_id), auth = credentials)
@@ -25,20 +32,42 @@ def request_patient(patient_id, credentials):
 
 
 
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+
+#     result = None
+#     credentials = (username, password)
+
+#     if request.method == 'POST':
+#         try:
+#             number = int(request.form['number'])
+#             result = request_patient(number, credentials=credentials)
+#         except ValueError:
+#             result = 'Invalid input. Please enter a number.'
+
+#     return render_template('index.html', result=result)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
     result = None
-    credentials = (username, password)
 
     if request.method == 'POST':
-        try:
-            number = int(request.form['number'])
-            result = request_patient(number, credentials=credentials)
-        except ValueError:
-            result = 'Invalid input. Please enter a number.'
+        has_ad = 'alzheimer' in request.form
+        has_memantine = 'memantine' in request.form
+
+        if has_ad and has_memantine:
+            fracture_percentage = fracture_risks['ad_memantine']
+        elif has_ad:
+            fracture_percentage = fracture_risks['ad_only']
+        else:
+            fracture_percentage = fracture_risks['no_ad_memantine']
+
+        result = {
+            'fracture_percentage': fracture_percentage
+        }
 
     return render_template('index.html', result=result)
+
 
 if __name__ == '__main__':
     port_str = os.environ['FHIR_PORT']
